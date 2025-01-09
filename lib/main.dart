c1f58d722313b709133af00b5bef4629d79d27eb
sky2000sh <sky2000sh@gmail.com>
2025-01-09 19:14:19 +0900

    Starting a Pomodoro app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 import 'package:toonflix/widgets/Button.dart';
 import 'package:toonflix/widgets/currency_card.dart';
 
@@ -170,108 +171,128 @@ void main() {
 // }
 
 // 20250107
-class App extends StatefulWidget {
+// class App extends StatefulWidget {
+//   const App({super.key});
+
+//   @override
+//   State<App> createState() => _AppState();
+// }
+
+// class _AppState extends State<App> {
+//   // // int counter = 0;
+//   // List<int> numbers = [];
+
+//   // void onClicked() {
+//   //   // 동적인 역할을 해줄 때는, 데이터의 변화를 위해서.
+//   //   // setState함수를 통해 build 메서드가 동작된다. = 기본적으로 build 메서드를 한 번 더 호출하는 역할을 한다.
+//   //   setState(() {
+//   //     // counter += 1;
+//   //     numbers.add(numbers.length);
+//   //     print('here's numbers :', numbers);
+//   //   });
+//   // }
+
+//   bool showTitle = true;
+
+//   void toggleTitle() {
+//     setState(() {
+//       showTitle = !showTitle;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         theme: ThemeData(
+//           textTheme: const TextTheme(
+//             titleLarge: TextStyle(
+//               color: Colors.red,
+//             ),
+//           ),
+//         ),
+//         home: Scaffold(
+//           backgroundColor: const Color(0xFFF4EDDB),
+//           body: Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 showTitle
+//                     ? const MyLargeTitle()
+//                     : const Text('Nothing to show!'),
+//                 IconButton(
+//                   onPressed: toggleTitle,
+//                   icon: const Icon(Icons.remove_red_eye),
+//                 ),
+//                 // Text(
+//                 //   '$counter',
+//                 //   style: const TextStyle(fontSize: 30),
+//                 // ),
+//                 // for (var n in numbers) Text('$n'),
+//                 // IconButton(
+//                 //   iconSize: 40,
+//                 //   onPressed: onClicked,
+//                 //   icon: const Icon(Icons.add_box_rounded),
+//                 // ),
+//               ],
+//             ),
+//           ),
+//         ));
+//   }
+// }
+
+// class MyLargeTitle extends StatefulWidget {
+//   const MyLargeTitle({
+//     super.key,
+//   });
+
+//   @override
+//   State<MyLargeTitle> createState() => _MyLargeTitleState();
+// }
+
+// class _MyLargeTitleState extends State<MyLargeTitle> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     print('initState');
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     print('dispose');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     print('build');
+//     return const Text(
+//       'My Large Title',
+//       style: TextStyle(
+//         fontSize: 30,
+//         // color: Theme.of(
+//         //   context.textTheme.titleLarge?.color,
+//         // ),
+//       ),
+//     );
+//   }
+
+// 20250109
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  // // int counter = 0;
-  // List<int> numbers = [];
-
-  // void onClicked() {
-  //   // 동적인 역할을 해줄 때는, 데이터의 변화를 위해서.
-  //   // setState함수를 통해 build 메서드가 동작된다. = 기본적으로 build 메서드를 한 번 더 호출하는 역할을 한다.
-  //   setState(() {
-  //     // counter += 1;
-  //     numbers.add(numbers.length);
-  //     print('here's numbers :', numbers);
-  //   });
-  // }
-
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFFF7626C),
           textTheme: const TextTheme(
-            titleLarge: TextStyle(
-              color: Colors.red,
+            // headlineLarge
+            displayLarge: TextStyle(
+              color: Color(0xFF232B55),
             ),
           ),
-        ),
-        home: Scaffold(
-          backgroundColor: const Color(0xFFF4EDDB),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                showTitle
-                    ? const MyLargeTitle()
-                    : const Text('Nothing to show!'),
-                IconButton(
-                  onPressed: toggleTitle,
-                  icon: const Icon(Icons.remove_red_eye),
-                ),
-                // Text(
-                //   '$counter',
-                //   style: const TextStyle(fontSize: 30),
-                // ),
-                // for (var n in numbers) Text('$n'),
-                // IconButton(
-                //   iconSize: 40,
-                //   onPressed: onClicked,
-                //   icon: const Icon(Icons.add_box_rounded),
-                // ),
-              ],
-            ),
-          ),
-        ));
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print('initState');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print('dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build');
-    return const Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        // color: Theme.of(
-        //   context.textTheme.titleLarge?.color,
-        // ),
-      ),
+          cardColor: const Color(0xFFF4EDDB)),
+      home: const HomeScreen(),
     );
   }
 }
